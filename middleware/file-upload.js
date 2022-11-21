00e75abb53524064005ec8f2afa1584902149fb4
Jason Tran <jasontran.pk@gmail.com>
2022-11-21 10:52:48 +0700

    change upload places to s3

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,6 +1,9 @@
 const multer = require('multer');
 const { v4: uuid } = require('uuid');
-const path = require('path');
+const { S3Client } = require('@aws-sdk/client-s3');
+const multerS3 = require('multer-s3');
+require('dotenv').config();
+
 const MIME_TYPE_MAP = {
 	'image/png': 'png',
 	'image/jpeg': 'jpeg',
@@ -9,7 +12,33 @@ const MIME_TYPE_MAP = {
 	'image/webp': 'webp',
 };
 
+const s3 = new S3Client({
+	region: 'ap-southeast-1',
+	credentials: {
+		accessKeyId: process.env.ACCESS_KEY_ID,
+		secretAccessKey: process.env.SECRET_ACCESS_KEY,
+	},
+});
+
 const fileUpload = multer({
+	limits: 5000000,
+	storage: multerS3({
+		s3: s3,
+		acl: 'public-read-write',
+		bucket: 'reactjs-placesharing',
+		key: function (req, file, cb) {
+			console.log(file);
+			const ext = MIME_TYPE_MAP[file.mimetype];
+			cb(null, 'uploads/images/' + uuid() + '.' + ext); //use Date.now() for unique file keys
+		},
+	}),
+	fileFilter: (req, file, cb) => {
+		const isValid = !!MIME_TYPE_MAP[file.mimetype];
+		let error = isValid ? null : new Error('Invalid mime type');
+		cb(error, isValid);
+	},
+});
+/* const fileUpload = multer({
 	limits: 5000000,
 	storage: multer.diskStorage({
 		destination: (req, file, cb) => {
@@ -25,6 +54,6 @@ const fileUpload = multer({
 		let error = isValid ? null : new Error('Invalid mime type');
 		cb(error, isValid);
 	},
-});
+}); */
 
 module.exports = fileUpload;
